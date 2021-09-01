b189a52ed12dcf041d82ed85cf9317c2a2b5e9fc
wojbiel833 <wojbiel833@gmail.com>
2021-09-01 16:35:17 +0200

    Add CRUD Operations testing

---
+++ models/test/department.crud.test.js
@@ -0,0 +1,156 @@
+const Department = require("../department.model");
+const expect = require("chai").expect;
+const mongoose = require("mongoose");
+
+describe("Department", () => {
+  before(async () => {
+    try {
+      await mongoose.connect("mongodb://localhost:27017/companyDBtest", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  describe("Reading data", () => {
+    before(async () => {
+      const testDepOne = new Department({ name: "Department #1" });
+      await testDepOne.save();
+
+      const testDepTwo = new Department({ name: "Department #2" });
+      await testDepTwo.save();
+    });
+
+    it('should return all the data with "find" method', async () => {
+      const departments = await Department.find();
+      const expectedLength = 2;
+
+      expect(departments.length).to.equal(expectedLength);
+    });
+
+    it('should return proper document by "name " with "findOne" method', async () => {
+      const department = await Department.findOne({ name: "Department #1" });
+      const expectedName = "Department #1";
+      expect(department.name).to.be.equal(expectedName);
+    });
+
+    after(async () => {
+      await Department.deleteMany();
+    });
+
+    describe("Creating data", () => {
+      //   it('should insert new document with "insertOne" method', async () => {
+      //     const department = new Department({ name: "Department #1" });
+      //     await department.save();
+      //     const savedDepartment = await Department({ name: "Department #1" });
+      //     expect(savedDepartment).not.to.be.null;
+      //   });
+      // LUB TAK
+      it('should insert new document with "insertOne" method', async () => {
+        const department = new Department({ name: "Department #1" });
+        await department.save();
+        expect(department.isNew).to.be.false;
+      });
+
+      after(async () => {
+        await Department.deleteMany();
+      });
+    });
+
+    describe("Updating data", () => {
+      beforeEach(async () => {
+        const testDepOne = new Department({ name: "Department #1" });
+        await testDepOne.save();
+
+        const testDepTwo = new Department({ name: "Department #2" });
+        await testDepTwo.save();
+      });
+
+      it('should properly update one document with "updateOne" method', async () => {
+        await Department.updateOne(
+          { name: "Department #1" },
+          { $set: { name: "=Department #1=" } }
+        );
+        const updatedDepartment = await Department.findOne({
+          name: "=Department #1=",
+        });
+        expect(updatedDepartment).to.not.be.null;
+      });
+
+      it('should properly update one document with "save" method', async () => {
+        const department = await Department.findOne({ name: "Department #1" });
+        department.name = "!Department #1!";
+        await department.save();
+
+        const updatedDepartment = await Department.findOne({
+          name: "!Department #1!",
+        });
+        expect(updatedDepartment).to.not.be.null;
+      });
+
+      //   it('should properly update multiple documents with "updateMany" method', async () => {
+      //     await Department.updateMany({}, { $set: { name: "!Updated!" } });
+
+      //     const updatedDepartments = await Department.find();
+      //     expect(updatedDepartments[0].name).to.be.equal("!Updated!");
+      //     expect(updatedDepartments[1].name).to.be.equal("!Updated!");
+      //   });
+      // LUB TAK
+      it('should properly update multiple documents with "updateMany" method', async () => {
+        await Department.updateMany({}, { $set: { name: "Updated!" } });
+        const departments = await Department.find({ name: "Updated!" });
+        expect(departments.length).to.be.equal(2);
+      });
+
+      afterEach(async () => {
+        await Department.deleteMany();
+      });
+    });
+
+    describe("Removing data", () => {
+      beforeEach(async () => {
+        const testDepOne = new Department({ name: "Department #1" });
+        await testDepOne.save();
+
+        const testDepTwo = new Department({ name: "Department #2" });
+        await testDepTwo.save();
+      });
+
+      //   it('should properly remove one document with "deleteOne" method', async () => {
+      //     await Department.deleteOne({ name: "Department #1" });
+      //     const dep = await Department.find();
+      //     expect(dep.length).to.equal(1);
+      //   });
+      // LUB
+      it('should properly remove one document with "deleteOne" method', async () => {
+        await Department.deleteOne({ name: "Department #1" });
+        const removeDepartment = await Department.findOne({
+          name: "Department #1",
+        });
+        expect(removeDepartment).to.be.null;
+      });
+
+      it('should properly remove one document with "remove" method', async () => {
+        const department = await Department.findOne({ name: "Department #1" });
+        await department.remove();
+
+        const removedDepartment = await Department.findOne({
+          name: "Department #1",
+        });
+        expect(removedDepartment).to.be.null;
+      });
+
+      it('should properly remove multiple documents with "deleteMany" method', async () => {
+        await Department.deleteMany();
+        const departments = await Department.find();
+        expect(departments.length).to.equal(0);
+      });
+
+      afterEach(async () => {
+        await Department.deleteMany();
+      });
+    });
+  });
+});
--- models/test/department.test.js
+++ models/test/department.test.js
@@ -8,41 +8,29 @@ describe("Department", () => {
     dep.validate((err) => {
       expect(err.errors.name).to.exist;
     });
-
-    after(() => {
-      mongoose.models = {};
-    });
   });
-
   it('should throw an error if "name" is not a string', () => {
     const cases = [{}, []];
     for (let name of cases) {
       const dep = new Department({ name });
-
       dep.validate((err) => {
         expect(err.errors.name).to.exist;
       });
     }
   });
-
   it("should throw an error if 'name' is too long or too short", () => {
     const cases = ["abc", "1234", "Lorem ipsum dolor amet"];
-
     for (let name of cases) {
       const dep = new Department({ name });
-
       dep.validate((err) => {
-        expect(err.error.name).to.exist;
+        expect(err.errors.name).to.exist;
       });
     }
   });
-
   it('should not throw an error if "name" is okay', () => {
     const cases = ["Management", "Human Resources"];
-
     for (let name of cases) {
       const dep = new Department({ name });
-
       dep.validate((err) => {
         expect(err).to.not.exist;
       });
